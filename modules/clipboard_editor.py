b4de845849edf38e03a61a17a623ced4902356e3
Tenku876 <poyo2c@gmail.com>
2024-02-18 18:38:54 +0900

    複数ページコピーの場合にページ表記が変わることを考慮できてなかったので対応。その他テスト簡略化

--- modules/clipboard_editor.py
+++ modules/clipboard_editor.py
@@ -7,7 +7,7 @@ class KindleEditor:
     @staticmethod
     def is_kindle(content: str) -> bool:
         """コンテントがキンドルのテキストか判定する。"""
-        regex = r".*\(p\.\d+\)\..*"  # (p.nnn). nは1桁以上の数値
+        regex = r".*\(pp?\.\d+-?\d*\)\..*"  # (p.nnn). or (pp.nnn-nnn)に対応 nは1桁以上の(数値|-)
         return bool(re.match(regex, content, re.DOTALL))
 
     @staticmethod
--- tests/test_clipboard_editor.py
+++ tests/test_clipboard_editor.py
@@ -6,52 +6,31 @@ from modules.clipboard_editor import KindleEditor
 class TestKindleEditor(unittest.TestCase):
 
     def test_is_kindle_true(self):
-        # 改行あり
-        kindle_data_nl = """
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true")) ​class UStaticMeshComponent* MeshComponent;
-
-Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p.197). Kindle 版. 
-        """
+        kindle_data_nl = "Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p.197). Kindle 版. "
         actual = KindleEditor.is_kindle(kindle_data_nl)
         self.assertTrue(actual)
-        # 改行なし
-        kindle_data_nonl = """
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-
-Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p.197). Kindle 版. """
-        actual = KindleEditor.is_kindle(kindle_data_nonl)
-        self.assertTrue(actual)
         # 他の本
-        kindle_data_otherbook = """
-Don’t Comment Bad Names — Fix the Names Instead
+        kindle_data_otherbook = "Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (p.80). O'Reilly Media. Kindle 版. "
+        actual = KindleEditor.is_kindle(kindle_data_otherbook)
+        self.assertTrue(actual)
+        # 複数ページに渡るケース"(ページ表記が変わる"
+        kindle_data_otherbook = "Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (pp.226-227). O'Reilly Media. Kindle 版. "
 
-Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (p.80). O'Reilly Media. Kindle 版. 
-"""
         actual = KindleEditor.is_kindle(kindle_data_otherbook)
         self.assertTrue(actual)
 
     def test_is_kindle_false(self):
-        # 改行あり
-        kindle_data_nl = """
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true")) ​class UStaticMeshComponent* MeshComponent;
-
-Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p.). Kindle 版. 
-        """
-        actual = KindleEditor.is_kindle(kindle_data_nl)
+        kindle_data_1 = "Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p.). Kindle 版. "
+        actual = KindleEditor.is_kindle(kindle_data_1)
         self.assertFalse(actual)
-        # 改行なし
-        kindle_data_nonl = """
-UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-
-Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p197). Kindle 版. """
-        actual = KindleEditor.is_kindle(kindle_data_nonl)
+        kindle_data_2 = "Ulibarri, Stephen. Unreal Engine C++ the Ultimate Developer's Handbook: Learn C++ and Unreal Engine by Creating a Complete Action Game (English Edition) (p197). Kindle 版."
+        actual = KindleEditor.is_kindle(kindle_data_2)
         self.assertFalse(actual)
         # 他の本
-        kindle_data_otherbook = """
-Don’t Comment Bad Names — Fix the Names Instead
-
-Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (p.80) O'Reilly Media. Kindle 版. 
-"""
+        kindle_data_otherbook = "Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (p.80) O'Reilly Media. Kindle 版. "
+        actual = KindleEditor.is_kindle(kindle_data_otherbook)
+        self.assertFalse(actual)
+        kindle_data_otherbook = "Boswell, Dustin; Foucher, Trevor. The Art of Readable Code: Simple and Practical Techniques for Writing Better Code (English Edition) (p.80-12) O'Reilly Media. Kindle 版. "
         actual = KindleEditor.is_kindle(kindle_data_otherbook)
         self.assertFalse(actual)
 
